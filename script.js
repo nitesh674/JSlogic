adfa99f2e5cff466dc3444fb0099db843ef7a7f0
nitesh674 <kurmaverma391@gmail.com>
2025-11-20 20:13:46 +0530

    question 2nd

--- script.js
+++ script.js
@@ -6,3 +6,19 @@
 
 // 2. Check for Palindrome
 
+function isPalindrome(str) {
+    let j = str.length - 1;
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[j]) {
+            return false;
+        }
+        j--;
+    }
+    return true;
+}
+let str1 = "racecar";
+let str2 = "nitin";
+let str3 = "Rama";
+console.log(str1 +isPalindrome( str1));
+console.log(str2 +isPalindrome( str2));
+console.log(str3 +isPalindrome( str3));
