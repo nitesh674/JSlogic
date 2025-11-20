3ae2d3af8bd1794635029f67eedb54c5220e0e4c
nitesh674 <kurmaverma391@gmail.com>
2025-11-20 20:03:32 +0530

    question 1 add

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let str = "hello";
-        result = str.split('').reverse().join('');
-        console.log(result);
+// 1. Reverse a String
+
+// let str = "hello";
+// result = str.split('').reverse().join('');
+// console.log(result);
+
+// 2. Check for Palindrome
+
